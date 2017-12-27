1a2f744f0e6c84fbf19d75f5d6100fb27801b616
dark1412myj <504112619@qq.com>
2017-12-27 14:04:31 +0800

    比较完整的mnist程序
    准确率 98.3%

--- MNist/mnist_test.py
+++ MNist/mnist_test.py
@@ -16,23 +16,16 @@ def get_matrix(shape,regularizer):
     return weights
 
 
-def inference(input_x,regularizer,aver):
+def inference(input_x,regularizer):
     with tf.variable_scope('layer1'):
         weighte1 = get_matrix([28*28,500],regularizer)
         bais1 = tf.get_variable('bais',[500],initializer=tf.random_normal_initializer(stddev=0.1))
-        if aver == None :
-            hide1 = tf.nn.relu( tf.matmul(input_x , weighte1) + bais1 )
-        else:
-            aver.apply([weighte1, bais1]) #这里要加一次滑动平均值给weight1和bais1的初始化，否则average为None，下行崩溃
-            hide1 = tf.nn.relu(tf.matmul(input_x, aver.average(weighte1) ) + aver.average(bais1))
+        hide1 = tf.nn.relu( tf.matmul(input_x , weighte1) + bais1 )
+
     with tf.variable_scope('layer2'):
         weighte2 = get_matrix([500,10],regularizer)
         bais2 = tf.get_variable('bais',[10],initializer=tf.random_normal_initializer(stddev=0.1))
-        if aver == None:
-            hide2 = tf.matmul(hide1,weighte2) + bais2        #得到输出层时不需要再做relu
-        else:
-            aver.apply([weighte2, bais2])
-            hide2 = tf.matmul(hide1, aver.average(weighte2) ) + aver.average(bais2)
+        hide2 = tf.matmul(hide1,weighte2) + bais2        #得到输出层时不需要再做relu
     return hide2
 
 
@@ -41,9 +34,7 @@ def train(mnist):
     x = tf.placeholder(tf.float32, [None, 28 * 28], 'input-x')
     y_ = tf.placeholder(tf.float32, [None, 10], 'input-y')
     global_step = tf.Variable(0,trainable=False)
-    var_averages = tf.train.ExponentialMovingAverage(0.9,global_step)
-    var_averages_op = var_averages.apply(tf.trainable_variables())
-    y = inference(x, regularizer,var_averages)
+    y = inference(x, regularizer)
     cross_entropy = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(labels=y_,logits=y))
     total_loss = cross_entropy + tf.get_collection('losses')
     train_step = tf.train.AdagradOptimizer(0.1).minimize(total_loss,global_step=global_step)
@@ -53,8 +44,8 @@ def train(mnist):
 
     #with tf.control_dependencies([train_step,var_averages_op]):
     #    train_op = tf.no_op(name='train')
-    with tf.control_dependencies([train_step]):
-        train_op = tf.no_op(name='train')
+    with tf.control_dependencies([train_step]):#定义一些列的动作，相当于绑定一些操作在一起
+        train_op = tf.no_op(name='train')      #定义空操作，因为该操作以来前面的，所以相当于前几个动作的连续执行
 
     saver = tf.train.Saver(max_to_keep=10)
     with tf.Session() as sess:
@@ -62,7 +53,6 @@ def train(mnist):
         for i in range(30000):
             xs,ys = mnist.train.next_batch(100)
             _,loss_value,step = sess.run( [train_op,cross_entropy,global_step],feed_dict={x:xs,y_:ys})
-            sess.run(var_averages_op)
             if i % 1000 == 0:
                 print(loss_value,'----:',step,sess.run(correct_rate,feed_dict={x:xs,y_:ys}))
                 saver.save(sess,os.path.join('./tfmodel/','test.ckpt'),global_step=global_step)
@@ -72,9 +62,7 @@ def evaluate(mnist):
     x = tf.placeholder(tf.float32, [None, 28 * 28], 'input-x')
     y_ = tf.placeholder(tf.float32, [None, 10], 'input-y')
     global_step = tf.Variable(0, trainable=False)
-    var_averages = tf.train.ExponentialMovingAverage(0.9, global_step)
-    var_averages_op = var_averages.apply(tf.trainable_variables())
-    y = inference(x, None, var_averages)
+    y = inference(x, None)
     saver = tf.train.Saver()
     correct_mat = tf.equal( tf.argmax(y,1),tf.argmax(y_,1) )
     correct_rate = tf.reduce_mean( tf.cast(correct_mat,tf.float32) )
